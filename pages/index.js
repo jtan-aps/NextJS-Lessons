ef102ac5869e057eed55679b65b173f978b5a957
jtan-aps <jtan4@aceproject.space>
2023-01-23 15:50:32 -0600

    Completed Lesson 74

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <ul></ul>
     </div>
   );
 }
