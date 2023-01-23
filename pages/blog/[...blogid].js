9f343dc4e48fa6e856e592edd5e5bd6b8d4db90e
jtan-aps <jtan4@aceproject.space>
2023-01-23 10:55:28 -0600

    Completed 62

--- pages/blog/[...blogid].js
+++ pages/blog/[...blogid].js
@@ -1,14 +1,14 @@
 import { useRouter } from "next/router";
 
-function ClientProjectsPage() {
+function BlogPostsPage() {
   const router = useRouter();
 
   console.log(router.query);
+
   return (
     <div>
-      <h1>The Projects of a given client</h1>
+      <h1>The Blog Posts</h1>
     </div>
   );
 }
-
-export default ClientProjectsPage;
+export default BlogPostsPage;
