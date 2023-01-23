5846e9f7905afe45767544ab720b0cf304213e30
jtan-aps <jtan4@aceproject.space>
2023-01-23 11:02:08 -0600

    Completed 63

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          {/* DONT DO THIS
+            <a href="/portfolio">Portfolio</a> */}
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+            <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
