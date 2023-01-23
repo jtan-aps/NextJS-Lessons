86f6abbd5773e55fc5161dca42ad33d1f42891a7
jtan-aps <jtan4@aceproject.space>
2023-01-23 11:18:29 -0600

    Completed 66

--- pages/clients/[clientid]/index.js
+++ pages/clients/[clientid]/index.js
@@ -4,9 +4,19 @@ function ClientProjectsPage() {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    router.push({
+        pathname: '/clients/[clientid]/[clientprojectid]',
+        query: {clientid: 'max', clientprojectid: 'projecta'}
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
