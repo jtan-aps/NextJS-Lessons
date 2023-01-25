678e0de8b3e297a697ee7c42911849b26b7ebbf8
jtan-aps <jtan4@aceproject.space>
2023-01-25 09:26:16 -0600

    Completed Lesson 85

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,7 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function SelectedEventPage() {
   const router = useRouter();
@@ -13,14 +14,23 @@ function SelectedEventPage() {
 
   const event = getEventById(eventId);
 
-  if (!event){
-    return <p>No event found!</p>
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
   }
-  
+
   return (
     <Fragment>
-      <EventSummary title={event.title}/>
-      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
