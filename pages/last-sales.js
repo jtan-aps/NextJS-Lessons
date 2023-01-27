2bedd5a328fb726c776b0f885fe75edb8cdfc267
jtan-aps <jtan4@aceproject.space>
2023-01-27 14:59:25 -0600

    Completed Lesson 114

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,36 +1,61 @@
 import { useEffect, useState } from "react";
 
+import useSWR from "swr";
+
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
+
+  const url =
+    "https://nextjs-course-8c50f-default-rtdb.firebaseio.com/sales.json";
+
+  const { data, error } = useSWR(url, (url) =>
+    fetch(url).then((response) => response.json())
+  );
 
   useEffect(() => {
-    setIsLoading(true);
+    if (data) {
+      const transformedSales = [];
 
-    fetch("https://nextjs-course-8c50f-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+  //   useEffect(() => {
+  //     setIsLoading(true);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  //     fetch("https://nextjs-course-8c50f-default-rtdb.firebaseio.com/sales.json")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to load</p>;
   }
 
-  if (!sales){
-    return <p>No data yet</p>
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
