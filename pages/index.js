54549ed3ddad08c63e3796a6574e07f462e3ba70
jtan-aps <jtan4@aceproject.space>
2023-01-27 11:50:13 -0600

    Completed Lesson 99

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import path from "path";
 import fs from "fs/promises";
 
+import Link from "next/link";
+
 function Home(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>
       ))}
     </ul>
   );
@@ -16,9 +18,7 @@ function Home(props) {
 export async function getStaticProps(context) {
   console.log("Regenerating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-
   const jsonData = await fs.readFile(filePath);
-
   const data = JSON.parse(jsonData);
 
   if (!data) {
