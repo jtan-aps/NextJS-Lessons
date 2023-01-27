04947113702084abb8e7585d20bb250de4969e6f
jtan-aps <jtan4@aceproject.space>
2023-01-27 15:12:04 -0600

    Completed Lesson 115

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 import useSWR from "swr";
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
   //   const [isLoading, setIsLoading] = useState(false);
 
   const url =
@@ -54,7 +54,7 @@ function LastSalesPage() {
     return <p>Failed to load</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -70,3 +70,27 @@ function LastSalesPage() {
 }
 
 export default LastSalesPage;
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-8c50f-default-rtdb.firebaseio.com/sales.json"
+  );
+
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return {
+    props: {
+      sales: transformedSales,
+    },
+  };
+}
