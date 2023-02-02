2ce04b5ffa42efa0353339deb65cec97c859c167
jtan-aps <jtan4@aceproject.space>
2023-02-02 14:20:13 -0600

    Completed Lesson 157

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -5,16 +5,33 @@ function handler(req, res) {
     // add server-side validation
     const { email, name, text } = req.body;
     if (
-      !email.includes("@") ||
+      !email.includes('@') ||
       !name ||
-      name.trime() === "" ||
+      name.trim() === "" ||
       !text ||
       text.trim() === ""
     ) {
       res.status(422).json({ message: "Invalid input" });
       return;
     }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+
+    res.status(201).json({ message: "Comment added", comment: newComment });
   } else if (req.method === "GET") {
+    const dummyList = [
+      { id: "C1", name: "Gabby", text: "First comment" },
+      { id: "C2", name: "Joe", text: "second comment" },
+    ];
+
+    res.status(200).json({ comments: dummyList });
   }
 }
 
