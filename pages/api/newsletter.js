741a5da2bddae706f2473b3ec4aca2c9e52c9f85
jtan-aps <jtan4@aceproject.space>
2023-02-03 10:50:18 -0600

    Completed Lesson 159

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,9 @@
-function handler(req, res) {
+//mongodb dbUser, qudLfQLanl9VGKrQ
+// mongodb+srv://dbUser:qudLfQLanl9VGKrQ@cluster0.xbdsnbp.mongodb.net/?retryWrites=true&w=majority
+
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -6,6 +11,16 @@ function handler(req, res) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://dbUser:qudLfQLanl9VGKrQ@cluster0.xbdsnbp.mongodb.net/?retryWrites=true&w=majority"
+    )
+    
+    const db = client.db();
+
+    await db.collection('emails').insertOne({email: userEmail});
+
+    client.close()
 
     console.log(userEmail);
     res.status(201).json({ message: "Email successfully added." });
