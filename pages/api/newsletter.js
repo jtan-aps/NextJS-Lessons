cd25846d0421c4c615e7dad56bd891453d3cf936
jtan-aps <jtan4@aceproject.space>
2023-02-03 11:11:06 -0600

    Completed Lesson 162

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -3,24 +3,45 @@
 
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://dbUser:qudLfQLanl9VGKrQ@cluster0.xbdsnbp.mongodb.net/?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection("emails").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
+
+    let client;
 
     if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://dbUser:qudLfQLanl9VGKrQ@cluster0.xbdsnbp.mongodb.net/?retryWrites=true&w=majority"
-    )
-    
-    const db = client.db();
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed" });
+      return;
+    }
 
-    await db.collection('emails').insertOne({email: userEmail});
-
-    client.close()
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed" });
+      return;
+    }
 
     console.log(userEmail);
     res.status(201).json({ message: "Email successfully added." });
