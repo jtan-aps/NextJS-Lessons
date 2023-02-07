11ba4070f037fd61e86a043c427e3ab26f8b627f
jtan-aps <jtan4@aceproject.space>
2023-02-07 08:21:22 -0600

    Completed Lesson 185

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -6,7 +6,7 @@ function PostsGrid(props) {
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostItem />
+        <PostItem post={posts} />
       ))}
     </ul>
   );
