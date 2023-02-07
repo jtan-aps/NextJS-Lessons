1e8168f12c59db769d6aa82cdac2c8a1519b5b75
jtan-aps <jtan4@aceproject.space>
2023-02-07 13:53:00 -0600

    Completed Lesson 192

--- content/posts/getting-started-with-nextjs.md
+++ content/posts/getting-started-with-nextjs.md
@@ -1,7 +1,7 @@
 ---
 title: "Getting Started with NextJS"
 date: "2023-03-08"
-image: "getting-started-with-nextjs.png"
+image: "getting-started-nextjs.png"
 excerpt: "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and comes with server-side rendering."
 isFeatured: true
 ---
--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,27 @@ import { Fragment } from "react";
 
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and comes with server-side rendering.",
-    date: "2022-02-10",
-  },  {
-    slug: "getting-started-with-nextjs2",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and comes with server-side rendering.",
-    date: "2022-02-10",
-  },  {
-    slug: "getting-started-with-nextjs3",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and comes with server-side rendering.",
-    date: "2022-02-10",
-  },  {
-    slug: "getting-started-with-nextjs4",
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and comes with server-side rendering.",
-    date: "2022-02-10",
-  },
-];
+function HomePage(props) {
+  const { posts } = props;
 
-function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
