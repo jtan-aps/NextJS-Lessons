98018f4e014e06a15aa2dbd89f0becf2ce82df14
jtan-aps <jtan4@aceproject.space>
2023-02-08 11:32:59 -0600

    Completed Lesson 200

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -21,7 +23,30 @@ function handler(req, res) {
       message,
     };
 
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://dbUser:qudLfQLanl9VGKrQ@cluster0.xbdsnbp.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db("my-site");
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage._id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed" });
+    }
+
     console.log(newMessage);
+
+    client.close();
 
     res
       .status(201)
