51eed9ef9f688f6d46ff8e43f1b7ae60ca081c17
jtan-aps <jtan4@aceproject.space>
2023-02-08 10:42:34 -0600

    Completed Lesson 196

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,8 +1,10 @@
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import Image from "next/image";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
-import Image from "next/image";
 
 // const DUMMY_POST = {
 //   slug: "getting-started-with-nextjs",
@@ -45,6 +47,20 @@ function PostContent(props) {
           </div>
         );
       }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
