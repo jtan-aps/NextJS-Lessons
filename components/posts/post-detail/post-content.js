a16e73115052088c4a67d2688a4248d2d3c95f34
jtan-aps <jtan4@aceproject.space>
2023-02-08 10:32:07 -0600

    Completed Lesson 195

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+import Image from "next/image";
 
 // const DUMMY_POST = {
 //   slug: "getting-started-with-nextjs",
@@ -13,12 +14,44 @@ import classes from "./post-content.module.css";
 
 function PostContent(props) {
   const { post } = props;
+
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
